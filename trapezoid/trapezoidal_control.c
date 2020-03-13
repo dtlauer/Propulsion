e3e7935afe92b75933bc3adcb931174acf9c7fb5
Dominic Lauer <dtlauer@mines.edu>
2020-03-13 10:37:55 -0600

    new folder for trapezoidal control

---
+++ trapezoid/trapezoidal_control.c
@@ -0,0 +1,18 @@
+// Dominic Lauer
+
+// data structure to hold Hall sensor data
+typedef struct {
+
+    uint8_t phaseU;
+    uint8_t phaseV;
+    uint8_t phaseW;
+
+} HallData
+
+// function to read in the Hall sensor data
+
+// function to determine switching states based on Hall sensor data
+
+// function to send signals to microcontroller
+
+// interrupt function
